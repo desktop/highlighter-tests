cbad83bab896018888b260e8d7534d087deffb0a
Wilhelm <wilhelm.wanecek@gmail.com>
2017-11-19 01:07:56 +0100

    Added Vue file

---
+++ vue.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ greeting }} World!</p>
+    <OtherComponent />
+  </div>
+</template>
+
+<script>
+import OtherComponent from './OtherComponent.vue';
+
+export default {
+  data() {
+    return {
+      greeting: 'Hello',
+    };
+  },
+
+  components: {
+    OtherComponent,
+  },
+};
+</script>
+
+<style lang="scss">
+$font-size: 2rem;
+p {
+  font-size: $font-size;
+  text-align: center;
+}
+</style>
