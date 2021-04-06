0fefa6a8a28e362b25d9125cd8283d1cefa6e3a8
Steven Yeh <say25@cornell.edu>
2021-04-06 10:45:40 -0500

    Make the CSharp Example more Robust
    - Add local functions
    - Add switch, if, for loop
    - Throw Exception

--- csharp.cs
+++ csharp.cs
@@ -1,12 +1,44 @@
 // A Hello World! program in C#.
+
 using System;
+using System.Collections.Generic;
+
 namespace HelloWorld
 {
-    class Hello
+    internal class Program
     {
-        static void Main()
+        /// <summary>
+        /// Entry Point to Program
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            static IEnumerable<int> GetOddSequenceEnumerator(int end)
+            {
+                if (end < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(end), "Must be larger than 0");
+                }
+
+                for (var i = 0; i <= end; i++)
+                {
+                    switch (i % 2)
+                    {
+                        case 1:
+                            yield return i;
+                            break;
+                        default:
+                            yield break;
+                    }
+                }
+            }
+
+            foreach (var i in GetOddSequenceEnumerator(100))
+            {
+                Console.WriteLine($"{i} is Odd.");
+            }
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
