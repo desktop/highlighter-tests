66bcbd52b40a017e6a3bef7522968deceae223e6
Dylan Ravel <dylanm.ravel@gmail.com>
2023-11-02 16:29:47 -0700

    Updated cmake dummy text

--- CMake.cmake
+++ CMake.cmake
@@ -1,12 +1,14 @@
-cmake_minimum_required(VERSION 3.1...3.27)
-
-project(
-  ModernCMakeExample
-  VERSION 1.0
-  LANGUAGES CXX)
-
-add_library(MyLibExample simple_lib.cpp simple_lib.hpp)
-
-add_executable(MyExample simple_example.cpp)
-
-target_link_libraries(MyExample PRIVATE MyLibExample)
+# vim: syntax=cmake
+if(NOT CMAKE_BUILD_TYPE)
+    # default to Release build for GCC builds
+    set(CMAKE_BUILD_TYPE Release CACHE STRING
+        "Choose the type of build, options are: None(CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug Release RelWithDebInfo MinSizeRel."
+        FORCE)
+endif()
+message(STATUS "cmake version ${CMAKE_VERSION}")
+if(POLICY CMP0025)
+    cmake_policy(SET CMP0025 OLD) # report Apple's Clang as just Clang
+endif()
+if(POLICY CMP0042)
+    cmake_policy(SET CMP0042 NEW) # MACOSX_RPATH
+endif()
